9f7fef543ecc0bbab62c8139af59cf65a84db325
Albert Cheng <lptalbert@gmail.com>
2020-02-05 19:22:24 -0800

    complete autoScale function

--- src/index.js
+++ src/index.js
@@ -12,8 +12,28 @@ const startGame = (ctx) => {
   game.bindKeyHandlers();
 }
 
-const resize = () => {
-  
+const autoScale = () => {
+  console.log("triggered");
+  const game_area = document.getElementById("game-canvas");
+  const width_height = 4 / 3;
+  const new_width = window.innerWidth;
+  const new_height = window.innerHeight;
+  const new_width_height = new_width / new_height;
+
+  if (new_width_height > width_height) {
+    new_width = new_height * width_height;
+    game_area.style.height = new_height + 'px';
+    game_area.style.width = new_width + 'px';
+  } else {
+    new_height = new_width / width_height;
+    game_area.style.width = new_width + 'px';
+    game_area.style.height = new_height + 'px';
+  }
+
+  const game_canvas = document.getElementById('game-canvas');
+  game_canvas.width = new_width;
+  game_canvas.height = new_height;
+
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -31,5 +51,8 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("game-over-menu").style.display = "none";
   };
   
-  
+  window.addEventListener('resize', autoScale, false);
+  window.addEventListener('orientationchange', autoScale, false);
+
 });
+
