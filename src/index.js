bbb272d6cf5db371edb066062d9d414138272633
Albert Cheng <lptalbert@gmail.com>
2020-02-07 15:50:21 -0800

    fix and complete rescaling

--- src/index.js
+++ src/index.js
@@ -12,35 +12,17 @@ const startGame = (ctx) => {
   game.bindKeyHandlers();
 }
 
-const autoScale = () => {
-  console.log("triggered");
-  const game_area = document.getElementById("game-canvas");
-  const width_height = 4 / 3;
-  const new_width = window.innerWidth;
-  const new_height = window.innerHeight;
-  const new_width_height = new_width / new_height;
-
-  if (new_width_height > width_height) {
-    new_width = new_height * width_height;
-    game_area.style.height = new_height + 'px';
-    game_area.style.width = new_width + 'px';
-  } else {
-    new_height = new_width / width_height;
-    game_area.style.width = new_width + 'px';
-    game_area.style.height = new_height + 'px';
-  }
-
-  const game_canvas = document.getElementById('game-canvas');
-  game_canvas.width = new_width;
-  game_canvas.height = new_height;
-
+const rescale = () => {
+  const zoom_percent = parseInt(window.innerWidth / 1900 * 100);
+  document.body.style.zoom = `${zoom_percent}%`;
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("game-canvas");
   const ctx = canvas.getContext("2d");
   loadGame(ctx);
-  
+  rescale();
+
   document.getElementById("start").onclick = () => {
     startGame(ctx);
     document.getElementById("start-menu").style.display = "none";
@@ -50,9 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
     startGame(ctx);
     document.getElementById("game-over-menu").style.display = "none";
   };
+
+  window.addEventListener('resize', rescale);
   
-  window.addEventListener('resize', autoScale, false);
-  window.addEventListener('orientationchange', autoScale, false);
-
 });
-
